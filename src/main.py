a27e30b7b31984641609d7d6d214101dc6661617
linnea-sedin <lsedin@chalmers.se>
2023-11-24 15:17:49 +0100

    Functions for serial and udp readings, not tested yet

--- src/main.py
+++ src/main.py
@@ -14,6 +14,7 @@ import argparse as ap # command line argument parser
 import csv # save data as csv file
 import socket
 import struct
+import serial
 
 # Define constants netft udp
 COMMAND_HEADER = 0x1234
@@ -25,9 +26,29 @@ CPF = 4448221.500
 CPT = 112984.8281
 LBF2N = 4.44822
 
+BAUDRATE = 115200
+
 udp_socket = None
+serial_port = None
+
+# Parse command line arguments
+# https://docs.python.org/3/library/argparse.html
+cArgsParser = ap.ArgumentParser(description='Process startup arguments.')
+cArgsParser.add_argument('-c', '--COM', action='store', help='Enter COM-port number as integer.', nargs=1, required=True, type=int)
+cArgsParser.add_argument('-p', '--PATH', action='store', default='', help='Enter filepath, without trailing "\\", or leave blank to save in same directory.', nargs=1, type=str) 
+
+cArgs = cArgsParser.parse_args()
+comport = cArgs.COM
+filepath = cArgs.PATH
+
+# Starting data collection
+print('Staring script, press Enter to exit...')
+print(f'Given COM-port: {comport}')
+print(f'Save path: {filepath}')
+print('Filename is: data_YYMMDD_HHMMSS.csv')
 
 
+# Neftft udp
 def setup_netft_udp():
     global udp_socket
     # Create a socket
@@ -66,45 +87,38 @@ def read_netft():
     return netft_data
 
 def close_socket():
-    udp_socket.close()   
+    udp_socket.close()
 
 
-
-if __name__ == "__main__":
-    ########## Parse command line arguments
-    # https://docs.python.org/3/library/argparse.html
-    cArgsParser = ap.ArgumentParser(description='Process startup arguments.')
-    cArgsParser.add_argument('-c', '--COM', action='store', help='Enter COM-port number as integer.', nargs=1, required=True, type=int)
-    cArgsParser.add_argument('-p', '--PATH', action='store', default='', help='Enter filepath, without trailing "\\", or leave blank to save in same directory.', nargs=1, type=str) 
-
-    cArgs = cArgsParser.parse_args()
-    comport = cArgs.COM
-    filepath = cArgs.PATH
-
-    ########## Starting data collection
-    print('Staring script, press Enter to exit...')
-    print(f'Given COM-port: {comport}')
-    print(f'Save path: {filepath}')
-    print('Filename is: data_YYMMDD_HHMMSS.csv')
-
-    # dummy data
-    dumbdata = [[123,100,200,1000],[456,110,210,1100]]
-
-    setup_netft_udp()
-
-    while True:
-        if kb.is_pressed('enter'):
-            print("Enter pressed. Exiting the loop. Goodbye!")
-            break
-
-        netft_data = read_netft()
-
-        print("Press Enter to exit.")
-        time.sleep(0.5)  # Optional: Add a small delay to avoid high CPU usage
-
-    close_socket()
+# Serial port
+def setup_arduino():
+    global serial_port
+    try:
+        serial_port = serial.Serial(comport, BAUDRATE)
+        return 1, serial_port
+    except Exception as e:
+        print("Error opening serial port!")
+        print(e)
+        return 0
     
-    ########## Save data to CSV file
+def read_arduino():
+    global serial_port
+    # arduino returns data after incoming "1" on serial
+    serial_port.write("1")
+    return serial_port.readline().decode('utf-8').strip()
+    
+def close_arduino():
+    global serial_port
+    try:
+        serial_port.close()
+        return 1
+    except Exception as e:
+        print("Error closing serial port!")
+        print(e)
+        return 0
+    
+def save_to_csv(data):
+    # Save data to CSV file
     fileTime = time.localtime()
     filename = 'data_' + str(fileTime.tm_year) + str(fileTime.tm_mon) + str(fileTime.tm_mday) + "_" + str(fileTime.tm_hour) + str(fileTime.tm_min) + str(fileTime.tm_sec) + '.csv'
 
@@ -117,4 +131,32 @@ if __name__ == "__main__":
         dataFile = csv.writer(dataFile, delimiter=';')
         
         dataFile.writerow(['Time', 'Vel1', 'Vel2', 'Force'])
-        dataFile.writerows(dumbdata)
+        dataFile.writerows(data)
+
+if __name__ == "__main__":
+    data = []
+
+    # Initialize
+    setup_netft_udp()
+    setup_arduino()
+
+    while True:
+        if kb.is_pressed('enter'):
+            print("Enter pressed. Exiting the loop. Goodbye!")
+            break
+        
+        arduino_data = read_arduino()
+        netft_data = read_netft()
+
+        # TODO: Add time stamp
+        data.append(arduino_data + netft_data)  # append row
+
+        print("Press Enter to exit.")
+        time.sleep(0.5)
+
+    save_to_csv(data)
+
+    close_socket()
+    close_arduino()
+
+    
