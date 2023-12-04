257d4a91739607e7cf77b58a40dcfe2151d53538
Oscar-Mark <osmark@student.chalmers.se>
2023-12-04 16:43:49 +0100

    Code is now working

--- OpticalSensors.py
+++ OpticalSensors.py
@@ -1,7 +1,8 @@
 import numpy as np
 import csv
 import pickle
-import datetime
+import time
+from pathlib import Path
 
 
 # Calibration method (one time, per sensor, depends on accuracy):
@@ -30,9 +31,9 @@ class CalibrationConfig:
         vRefX, vRefY = vRefs
         self.alpha = 0
         self.alphaVar = 0
-        self.bias = np.array([0][0])  # for original frame
-        self.scale = np.array([[1][1]])  # for original frame
-        self.frame = np.array([[1, 0][0, 1]])
+        self.bias = np.zeros(shape=(2,1))  # for original frame
+        self.scale = np.ones(shape=(2,1))  # for original frame
+        self.frame = np.eye(2)
 
         self._unpack_data(calibDataPaths)
         self._calculate_bias(self.staticData)
@@ -43,26 +44,41 @@ class CalibrationConfig:
     def _unpack_data(self, rawCalibData):
         # Unpacks tuple of strings (file paths) and reads csv into np array
         staticPath, constVelXPath, constVelYPath = rawCalibData
-        self.staticData = np.genfromtxt(staticPath, delimiter=",")
-        self.constVelXData = np.genfromtxt(constVelXPath, delimiter=",")
-        self.constVelYData = np.genfromtxt(constVelYPath, delimiter=",")
-
+        self.staticData = np.genfromtxt(staticPath, delimiter=",", skip_header=1)
+        self.constVelXData = np.genfromtxt(constVelXPath, delimiter=",", skip_header=1)
+        self.constVelYData = np.genfromtxt(constVelYPath, delimiter=",", skip_header=1)
+        if np.isnan(self.staticData).any():
+            ValueError("Static data contains NaN values")
+        if np.isnan(self.constVelXData).any():
+            ValueError("Const vel X data contains NaN values")
+        if np.isnan(self.constVelYData).any():
+            ValueError("Const vel Y data contains NaN values")
+            
     def _calculate_bias(self, staticData: dict):
-        self.bias = np.transpose(np.mean(staticData[:][0:1], axis=0))
+        self.bias[0] = np.mean(staticData[:,0], axis=0)
+        self.bias[1] = np.mean(staticData[:,1], axis=0)
+        if self.bias.shape != (2,1):
+            IndexError("Dimensions of bias not correct")
 
     def _calculate_scale(self, constVelData: dict, vRef: float, axis: int):
-        vConstMean = np.transpose(np.mean(constVelData, axis=0))
-        self.scale[axis] = vRef / vConstMean[axis]
+        vConstMean = np.transpose(np.mean(constVelData[:,axis], axis=0))
+        self.scale[axis] = vRef / vConstMean
+        if self.scale.shape != (2,1):
+            IndexError("Dimensions of scale not correct")
 
     def _calculate_transformation(self, constVelXData):
-        alpha = np.arctan2(constVelXData[:][1], constVelXData[:][0])
+        alpha = np.arctan2(constVelXData[:,1], constVelXData[:,0])
         self.alpha = np.mean(alpha)  # let alpha be mean of measurements
         self.alphaVar = np.var(alpha)  # save variance of alpha for analysis
-        self.frame = np.array([np.cos(self.alpha), np.sin(self.alpha)],
-                              [np.sin(self.alpha), -np.cos(self.alpha)])
+        self.frame = np.array([[np.cos(self.alpha), np.sin(self.alpha)],
+                              [-np.sin(self.alpha), np.cos(self.alpha)]])
+        if self.frame.shape != (2,2):
+            IndexError("Dimensions of transformation not correct")
 
     def save_to_file(self, filename: str):
-        with open(filename, 'wb') as file:
+        filepath = Path(".") / "CalibConfigs" / filename
+        print(filepath)
+        with filepath.open('wb') as file:
             pickle.dump(self, file)
 
     def unpack(self):
@@ -70,26 +86,28 @@ class CalibrationConfig:
 
 
 class OpticalSensorReading:
-    def __init__(self, rawDataPath: dict, calibDataPaths=None, calibConfigs=None, d=5):
+    def __init__(self, rawDataPath: dict, calibDataPaths=None, vRefs=None, calibConfigs=None, d=5):
         self.d = d
         if calibDataPaths is not None:
-            self._calibrate_sensors(calibDataPaths)
+            self._calibrate_sensors(calibDataPaths, vRefs)
             self._save_calib_config()
         else:
             self._read_config(calibConfigs)
         self.rawDataPath = rawDataPath
         self.transformedData = None
+
+        self._read_data()
         self._transform_readings()
 
-    def _calibrate_sensors(self, calibDataPaths):
-        calibDataL, calibDataR = calibDataPaths
-        self.calibConfigL = CalibrationConfig(calibDataL)
-        self.calibConfigR = CalibrationConfig(calibDataR)
+    def _calibrate_sensors(self, calibDataPaths, vRefs):
+       # calibDataL, calibDataR = calibDataPaths
+        self.calibConfigL = CalibrationConfig(calibDataPaths, vRefs)
+        self.calibConfigR = CalibrationConfig(calibDataPaths, vRefs)
 
     def _save_calib_config(self, filename='calibConfig'):
-        time = str(datetime.datetime.now())
-        self.calibConfigL.save_to_file(filename + '_L_' + time)
-        self.calibConfigR.save_to_file(filename + '_R_' + time)
+        timestr = time.strftime("%Y%m%d-%H%M%S")
+        self.calibConfigL.save_to_file(filename + '_L_' + timestr + '.pkl')
+        self.calibConfigR.save_to_file(filename + '_R_' + timestr + '.pkl')
 
     def _read_config(self, calibConfigs: tuple):
         # Read config files from tuple of path names
@@ -101,30 +119,36 @@ class OpticalSensorReading:
 
     def _read_data(self):
         # Turn raw CSV into np-array
-        self.rawData = np.genfromtxt(self.rawData, delimiter=",")
+        self.rawData = np.genfromtxt(self.rawDataPath, delimiter=",", skip_header=1)
+        if np.isnan(self.rawData).any():
+            ValueError("Raw data contains NaN values")
 
     def _transform_readings(self):
         # Let data have columns t/dxL/dyL/dxR/dyR/...
         data = self.rawData
-        bData = np.zeros(shape=(len(data, 0), 5))
+        bData = np.zeros(shape=(data.shape[0], 5))
         biasL, scaleL, frameL = self.calibConfigL.unpack()
         biasR, scaleR, frameR = self.calibConfigR.unpack()
-        for t in range(len(data, 0)):
-            t = data[t][0]
-            if t == 0:
-                dt = np.inf()
+        for row in range(data.shape[0]):
+            t = data[row,0]
+            if row == 0:
+                dt = 1000000.00
             else:
-                dt = data[t][0] - data[t - 1][0]
-            bData[t][2:3] = [t, dt]
-            vL = scaleL * (np.transpose(data[t][1:2]) - biasL) / dt
+                dt = data[row,0] - data[row-1,0]
+            bData[row,0] = t
+            bData[row,1] = dt
+            vL = scaleL * (np.transpose(data[row,1:2]) - biasL) / dt
             vL = frameL @ vL
-            vR = scaleR * (np.transpose(data[t][3:4]) - biasR) / dt
+            vR = scaleR * (np.transpose(data[row,3:4]) - biasR) / dt
             vR = frameR @ vR
-            bData[t][2:3] = np.mean([vL],[vR],axis=1)
-            bData[t][4] = (vL[1] - vR[1]) / (2 * self.d)
+            vMean = np.mean(np.concatenate((vL,vR),axis=1),axis=1)
+            bData[row,2] = vMean[0]
+            bData[row,3] = vMean[1]
+            bData[row,4] = (vL[1] - vR[1]) / (2 * self.d)
         self.transformedData = bData
 
     def getData(self):
-        if self.transformedData == None:
+        if self.transformedData is None:
             ValueError("Was not able to transform readings.")
         return self.transformedData
+    
